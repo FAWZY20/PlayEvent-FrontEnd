187922bb76965396553803c943924e6ad8d08b6e
fawzy20 <fawzy-elsam@outlook.fr>
2024-10-23 18:05:40 +0200

    feat(refactor): page event

--- src/app/service/search-song.service.ts
+++ src/app/service/search-song.service.ts
@@ -7,14 +7,21 @@ import { Observable } from 'rxjs';
 })
 export class SearchSongService {
   songUrl: string;
+  songText: string;
 
   constructor(
     private http: HttpClient
   ) {
     this.songUrl = "https://cors-anywhere.herokuapp.com/https://api.deezer.com/search?q="
+    this.songText = "https://lyrist.vercel.app/api"
   }
 
   searchSong(search: string): Observable<any> {
     return this.http.get(this.songUrl + search)
   }
+
+  getSongText(artist: string, son: string): Observable<any> {
+   return this.http.get(this.songText + `/${son}/${artist}`)
+  }
+
 }
